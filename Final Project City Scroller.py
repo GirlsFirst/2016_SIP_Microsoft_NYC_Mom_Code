af2385d7d8683b083d3b42e039676bc89e0ba4bc
Dominique Eversley <dominique267@ebtbrooklyn.com>
2016-08-15 15:48:21 -0400

    got the tips to appear

--- Final Project City Scroller.py
+++ Final Project City Scroller.py
@@ -36,14 +36,12 @@ backgroundim = pygame.image.load("bg3.png").convert()
 xPosition = 400
 yPosition = 15
 
-
 all_sprites_list = pygame.sprite.Group()
 bad_sprites_list = pygame.sprite.Group()
 good_sprites_list = pygame.sprite.Group()
-envelope_sprites_list = pygame.sprite.Group()
-lives = 1
-
+envelope_sprite_list = pygame.sprite.Group()
 sb_list = pygame.sprite.Group()
+
 class girlSprite(pygame.sprite.Sprite):
 
     def __init__(self, file_name, file_name2):
@@ -57,6 +55,7 @@ class girlSprite(pygame.sprite.Sprite):
         self.image = pygame.image.load(self.images[self.index]).convert_alpha()
         self.rect = self.image.get_rect()
 
+
     def changesize(self, width, height):
         center = self.rect.center
         self.width = width
@@ -64,12 +63,12 @@ class girlSprite(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(self.image, (self.width, self.height))
         self.rect = self.image.get_rect()
         self.rect.center = center
-    
+
     def playerjump(self):
         if (pygame.key.get_pressed()[pygame.K_SPACE] == 1):
-            self.rect.y = 150
-        if (pygame.key.get_pressed()[pygame.K_SPACE] == 0 and self.rect.y != 500):
-            self.rect.y = 450
+            self.rect.y = 250
+        if (pygame.key.get_pressed()[pygame.K_SPACE] == 0 and self.rect.y != 450):
+            self.rect.y = 400
             
     def update(self):
         self.rect.x -= 3
@@ -82,10 +81,9 @@ class girlSprite(pygame.sprite.Sprite):
         if self.index >= len(self.images):
             self.index = 0
         self.image = pygame.image.load(self.images[self.index]).convert_alpha()
- 
 
 def makeup():
-    for i in range(random.randrange(1, 3)):
+    for i in range(1):
         germ_sprite = girlSprite("germ.png", "germ.png")
         troll_sprite = girlSprite("evillegs.png", "evillegs.png")
         honey_sprite = girlSprite("honey.png", "honey.png")       
@@ -120,26 +118,23 @@ def makeup():
         all_sprites_list.add(germ_sprite)
         all_sprites_list.add(troll_sprite)
         all_sprites_list.add(honey_sprite)
+        all_sprites_list.add(envelope_sprite)
         all_sprites_list.add(diaper_sprite)
         all_sprites_list.add(pacifier_sprite)
-        envelope_sprites_list.add(envelope_sprite)
-        
+        envelope_sprite_list.add(envelope_sprite)
 
-
-        
 def end():
-
-    
     good_sprites_list.empty()
     all_sprites_list.empty()
     bad_sprites_list.empty()
-    envelope_sprites_list.empty()
+    envelope_sprite_list.empty()
 
 
 lives = 3
 points = 0
+tipnumber = 0
 
-sprite1 = girlSprite("girlstop1.png", "rungirl.png")
+sprite1 = girlSprite("girlstop.png", "rungirl.png")
 baby_sprite = girlSprite("baby1.png", "baby1.png")
 scoreboard_sprite = girlSprite("scoreboard.png", "scoreflash.png")
 
@@ -147,7 +142,8 @@ all_sprites_list.add(sprite1)
 all_sprites_list.add(baby_sprite)
 sb_list.add(scoreboard_sprite)
 
-
+scoreboard_sprite.rect.x = 600
+scoreboard_sprite.rect.y = 150
 
 sprite1.rect.x = 30
 sprite1.rect.y = 450
@@ -155,8 +151,7 @@ sprite1.rect.y = 450
 baby_sprite.rect.x = 700
 baby_sprite.rect.y = 450
 
-scoreboard_sprite.rect.x = 600
-scoreboard_sprite.rect.y = 150
+
 while not done:   
    # --- Main event loop
     for event in pygame.event.get():
@@ -169,8 +164,11 @@ while not done:
                 points = 0
                 makeup()
                 restart = False
-
-            
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_g and over:
+                pygame.quit()
+                exit() 
+                
     sprite1.playerjump()    
     bad_sprites_list.update()
     sprite1.animate()
@@ -179,19 +177,18 @@ while not done:
     sprite1.changesize(150, 150)
     scoreboard_sprite.changesize(300, 300)
     baby_sprite.changesize(150, 150)
-    envelope_sprites_list.update()
+    envelope_sprite_list.update()
 
 
     bad_sprites_hit_list = pygame.sprite.spritecollide(sprite1, bad_sprites_list, True)
     good_sprites_hit_list = pygame.sprite.spritecollide(sprite1, good_sprites_list, True)
-    envelope_sprites_hit_list = pygame.sprite.spritecollide(sprite1, envelope_sprites_list, True)      
+    envelope_sprites_hit_list = pygame.sprite.spritecollide(sprite1, envelope_sprite_list, True)
 
     
     # --- Game logic should go here
     badguy = len(bad_sprites_list)
     if badguy <= 2:
         makeup()
-
         
 
     # --- Screen-clearing code goes here
@@ -216,10 +213,10 @@ while not done:
     front_scroller.move_buildings()
     sidewalk.draw(screen)
 
-
     all_sprites_list.draw(screen)
     sb_list.draw(screen)
-    mom_code_tip0 = ["Figuring out what your baby is upset about is major guessing game(no pun intended).",
+
+    mom_code_tips = ["Figuring out what your baby is upset about is major guessing game(no pun intended).",
      "After having to guess if they are hunger,messy, or just want some attention another factor could be that your baby is gassy.",
      "When coming out of the womb babies are born with the Moro, or startle,reflex.",
      "If your baby's private parts are a bit bigger than usual it might be due to swelling caused by pressure exerted on your baby during birth.",
@@ -228,8 +225,7 @@ while not done:
      "Breast milk is more quickly digested and more completely absorbed than formula.",
      "Skin-to-skin contact with your baby allows you and your baby to build a stronger bond.",
      "When making eye contact with your baby it will allow your baby to recognize your face and starts to build their memory.",
-     "While many may think honey would be a good thing to feed to your newborn it is actually contains a bacteria that can germinate in a baby’s developing digestive system."
-     "When having a newborn to worry about be extra alert when driving if your daily routine changes.",
+     "While many may think honey would be a good thing to feed to your newborn it is actually contains a bacteria that can germinate in a baby’s developing digestive system.",
      "Save yourself from the despair of buying a changing table it becomes virtually useless when your baby has to be changed outside the house.",
      "Changing pads are more efficient than changing tables because you can take them everywhere.", 
      "Wipe warmers are also something you can refrain from buying because they tend to dry out wipes faster.",
@@ -240,35 +236,44 @@ while not done:
      "Touching your baby’s soft spot is not a big deal you’re not touching their brain but a thick protective  membrane.",
      "Babies  who are fed formula have less-frequent bowel movements which causes them to poop less."]
 
-    length = len(mom_code_tip0)
+    length = len(mom_code_tips)
     random_number = random.randint(0, length -1)
-    (mom_code_tip0 [random_number])
+    #(mom_code_tip0 [random_number])
 
-    font = pygame.font.Font(None, 50)
-    
+    font1 = mom_code_tips.pygame.font.Font(None, 50)
+
     score = font.render("Lives: "+ str(lives), True, BLACK)
     screen.blit(score, [600, 150])
 
-    tips = font.render("Score: " + str(points), True, BLUE)
-    screen.blit(tips, [600, 200])
-
-    if good_sprites_hit_list:
-        points +=1
+    tips = font.render( "Score: " + str(points), True, BLACK)
+    screen.blit(tips, [600, 200])  
+    
+    ln_ = mom_code_tips[tipnumber]
+    
 
     if bad_sprites_hit_list:
         lives -= 1
-                
+        
     if lives < 1:
         end()
         game_over = font.render("Game Over", True, RED)
         screen.blit(game_over, [300, 200])
         try_again = font.render("Play Again and Press A", True, RED)
-        screen.blit(try_again, [300, 350])
+        screen.blit(try_again, [200, 300])
         restart = True
-
-    if envelope_sprites_hit_list:
-        points +=1
+        endit = font.render("End Game and Press G", True, RED)
+        screen.blit(endit, [200, 400])
+        over = True 
+        tip_it = font.render(ln_, True, BLACK)
+        screen.blit(tip_it, [100, 500])
         
+        
+        
+    if envelope_sprites_hit_list:
+        points += 1
+        tipnumber += 1
+
+
         
 
 
